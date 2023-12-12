71e2d3260a6efae9f8902cc4bf0569ad30e33bf4
Sumit Parashar <145322453+THEELITE100@users.noreply.github.com>
2023-12-12 15:55:11 +0530

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,7 @@
+#mad libs game
+color=input("Enter a color: ")
+plural_noun=input("Enter a Plural Noun: ")
+celebrity=input("Enter a celebrity: ")
+print("Roses are "+color)
+print(plural_noun+" is blue")
+print("I Love "+celebrity)
