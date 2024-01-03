ad82c8a341b2f7c17cb1eac4845c7ece128966ad
Sumit Parashar <145322453+THEELITE100@users.noreply.github.com>
2024-01-03 20:03:32 +0530

    Delete Mad Libs Game.py

--- Mad Libs Game.py
+++
@@ -1,7 +0,0 @@
-#mad libs game
-color=input("Enter a color: ")
-plural_noun=input("Enter a Plural Noun: ")
-celebrity=input("Enter a celebrity: ")
-print("Roses are "+color)
-print(plural_noun+" is blue")
-print("I Love "+celebrity)
